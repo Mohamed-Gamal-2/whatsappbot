1c4ea6b39912186a618e0ada252b41e60a0975b7
Ellithy <engmohamedellithy13@gmail.com>
2023-11-30 21:14:45 +0200

    update client controller

--- Module/ClientModule/client.controller.js
+++ Module/ClientModule/client.controller.js
@@ -1,12 +1,43 @@
 import { client } from "../../index.js";
+
+// Use an object to store information about users
+const userMap = {};
+
 function reply() {
   client.on("message", (message) => {
     console.log(message);
-    let clientID = message._data.id.remote;
-    if (message.body === "!ping") message.reply("pong");
-    else if (message.body === "Hi")
-      client.sendMessage(clientID, "Hello there, How can I help you?");
+
+    // Extracting sender's ID
+    const clientID = message.from;
+    console.log("usermap", userMap[clientID]);
+    // Check if it's the first time the user is sending a message
+    if (!userMap[clientID]) {
+      // Set a flag to indicate that the user has sent a message
+      userMap[clientID] = true;
+
+      // Send the initial message for the first interaction
+      client.sendMessage(
+        clientID,
+        "Hello! This is your first message. How can I assist you?"
+      );
+      return;
+    }
+
+    // Process other messages
+    handleMessage(message);
   });
+}
+
+function handleMessage(message) {
+  // Handle different message cases
+  if (message.body.toLowerCase() === "!ping") {
+    // Respond to !ping with "pong"
+    message.reply("pong");
+  } else if (message.body.toLowerCase() === "hi") {
+    // Respond to "Hi" with a greeting
+    client.sendMessage(message.from, "Hello there! How can I help you?");
+  }
+  // Add more message handling cases if needed
 }
 
 export { reply };
