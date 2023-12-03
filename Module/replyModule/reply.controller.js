008d6a994d23ecc057199ec0c9ef595a1646d27a
Mohamed-Gamal-2 <141522329+Mohamed-Gamal-2@users.noreply.github.com>
2023-12-03 22:29:10 +0200

    feature: adding middleware + replying messages to media

--- Module/replyModule/reply.controller.js
+++ Module/replyModule/reply.controller.js
@@ -1,7 +1,11 @@
 import replyModel from "../../Database/Schema/replySchema.js";
 import { client } from "../ClientModule/client.controller.js";
-
+import fs from "fs";
+import pkg from "whatsapp-web.js";
+import { fileName as filename } from "../../middleware/middleware.upload.js";
 const userMap = {};
+
+const { MessageMedia } = pkg;
 
 function reply() {
   client.on("message", (message) => {
@@ -25,61 +29,95 @@ function reply() {
   });
 }
 
- async function handleMessage(message) {
-    const msg = await replyModel.findOne({message: message.body})
-    if (msg){
-        message.reply(msg.reply)
-    }else{
+async function handleMessage(message) {
+  const msg = await replyModel.findOne({ message: message.body });
+  const PDFRegex = /\.PDF$/i;
+  const imageRegex =
+    /\.(PNG|JPEG|JPG|GIF|TIFF|TIF|BMP|SVG|WEBP|ICO|RAW|PSD|EPS|AI)$/i;
+  const VideoRegex = /\.(MP4|AVI|MKV|WMV|MOV|FLV|MPEG|WEBM|OGV|MPG)$/i;
+  if (msg) {
+    if (PDFRegex.test(msg.reply)) {
+      const dataBuffer = fs.readFileSync(`./media/${msg.reply}`);
+      const base64PDF = dataBuffer.toString("base64");
+      const media = new MessageMedia("application/pdf", base64PDF);
+      client.sendMessage(message.from, media);
+    } else if (imageRegex.test(msg.reply)) {
+      const dataBuffer = fs.readFileSync(`./media/${msg.reply}`);
+      const base64Image = dataBuffer.toString("base64");
+      const media = new MessageMedia(
+        `image/${msg.reply.slice(msg.reply.lastIndexOf("."))}`,
+        base64Image
+      );
+      client.sendMessage(message.from, media);
+    } else if (VideoRegex.test(msg.reply)) {
+      const dataBuffer = fs.readFileSync(`./media/${msg.reply}`);
+      const base64video = dataBuffer.toString("base64");
+      const media = new MessageMedia(
+        `video/${msg.reply.slice(msg.reply.lastIndexOf("."))}`,
+        base64video
+      );
+      client.sendMessage(message.from, media);
+    } else {
+      message.reply(msg.reply);
+    }
+  } else {
     client.sendMessage(message.from, "please, choose option form list");
-       
+  }
+}
+async function addReply(req, res) {
+  try {
+    if (!filename) {
+      const reply = await replyModel.insertMany([req.body]);
+      res.json({ message: "reply added", reply });
+    } else {
+      const reply = await replyModel.insertMany([
+        { reply: filename, message: req.body.message },
+      ]);
+      res.status(200).json({ message: "media uploaded" });
     }
+  } catch (error) {
+    if (error.errmsg?.includes("E11000 duplicate key")) {
+      res.json({
+        message: "This message exists before , write unique message",
+      });
+    } else {
+      res.json({ message: "error", error });
+    }
+  }
 }
 
-async function addReply(req,res){
-    try{
-        const reply = await replyModel.insertMany([req.body])
-        res.json({ message: "reply added" , reply})
-    }catch(error){
-      if(error.errmsg?.includes("E11000 duplicate key")){
-          res.json({
-            message: "This message exists before , write unique message",
-          });
-      }else{
-          res.json({ message: "error" , error})
-        }
+async function updateReply(req, res) {
+  try {
+    const { reply, message } = req.body;
+    if (!message) {
+      res.json({ message: " Enter your message" });
+    } else {
+      const updatedReply = await replyModel.findOneAndUpdate(
+        { message },
+        { reply },
+        { new: true }
+      );
+      if (!updatedReply) {
+        res.json({ message: "message not found" });
+      }
+      res.json({ message: "7mo", updatedReply });
     }
+  } catch (error) {
+    res.json({ message: "error", error });
+  }
 }
 
-async function updateReply(req,res){
-    try{
-        const {reply,message}=req.body
-        if(!message){
-            res.json({ message: " Enter your message" })
-        }else{
-
-            const updatedReply =await replyModel.findOneAndUpdate({message},{reply},{new:true})
-            if (!updatedReply){
-                res.json({ message: "message not found"});
-            }
-            res.json({ message: "7mo", updatedReply });
-        }
-        }catch(error){
-            
-            res.json({ message: "error" , error})
-        }
-        
+async function getAllreplies(req, res) {
+  try {
+    const allreplies = await replyModel.find();
+    if (allreplies) {
+      res.json({ message: "All replies", allreplies });
+    } else {
+      res.json({ message: " No replies founded" });
     }
-    
-    async function getAllreplies(req,res){
-        try{
-            const allreplies = await replyModel.find()
-            if (allreplies){
-                res.json({message: "All replies" ,allreplies})
-            }else{
-                res.json({message: " No replies founded" })
-            }
-        }catch{
-        res.json({ message: "error" , error})
-    }
+  } catch (error) {
+    res.json({ message: "error", error });
+  }
 }
+
 export { reply, addReply, updateReply, getAllreplies };
--- Module/replyModule/reply.route.js
+++ Module/replyModule/reply.route.js
@@ -1,28 +1,13 @@
 import express from "express";
-import multer from "multer";
-import path from "path"
-import { addReply, getAllreplies, updateReply } from "./reply.controller.js";
 
+import { addReply, getAllreplies, updateReply } from "./reply.controller.js";
+import { uploadingMediaMW } from "../../middleware/middleware.upload.js";
 
 const replyRouter = express.Router();
 
-// upload
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join("E:/web-project/whatsapp bot/whatsappbot/media"));
-  },
-  filename: function (req, file, cb) {
-    cb(null, new Date().toISOString().replace(/:/g,"-") + file.originalname);
-  },
-});
-
-const uploadMedia = multer({ storage });
-
-replyRouter.post("/add", addReply);
+replyRouter.post("/add", uploadingMediaMW, addReply);
 replyRouter.patch("/update", updateReply);
 replyRouter.get("/all", getAllreplies);
-replyRouter.post("/upload", uploadMedia.single("media"), (req, res) => {
-  res.status(200).json({ message: "media uploaded" });
-});
+// replyRouter.post("/upload", uploadMedia.single("media"), uploadingMedia);
 
 export { replyRouter };
