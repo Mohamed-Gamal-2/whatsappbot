ce22cf7d7e516d6efbbadbf9c64271f4e8d9c2d1
Ellithy <engmohamedellithy13@gmail.com>
2023-12-03 17:18:47 +0200

    add multer pkg

--- Module/replyModule/reply.route.js
+++ Module/replyModule/reply.route.js
@@ -1,11 +1,28 @@
 import express from "express";
+import multer from "multer";
+import path from "path"
 import { addReply, getAllreplies, updateReply } from "./reply.controller.js";
 
+
 const replyRouter = express.Router();
+
+// upload
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join("E:/web-project/whatsapp bot/whatsappbot/media"));
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString().replace(/:/g,"-") + file.originalname);
+  },
+});
+
+const uploadMedia = multer({ storage });
 
 replyRouter.post("/add", addReply);
 replyRouter.patch("/update", updateReply);
 replyRouter.get("/all", getAllreplies);
-
+replyRouter.post("/upload", uploadMedia.single("media"), (req, res) => {
+  res.status(200).json({ message: "media uploaded" });
+});
 
 export { replyRouter };
